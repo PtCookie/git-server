72e6aeaf2f887a9638b9fbfe8eecd9e3870bb419
Minsup Ju <dev@ptcookie.dev>
2024-05-07 00:44:56 +0900

    style(fmt): Reformat shell script

--- 10-setup.sh
+++ 10-setup.sh
@@ -4,53 +4,53 @@
 set -eu
 
 warn() {
-    echo "$0: WARN: $@"
+	echo "$0: WARN: $@"
 }
 
 # Set specific UID and GID for the git user
 if [ -n "${GIT_USER_UID-}" ]; then
 
-    if [ -z "${GIT_USER_GID-}" ]; then
-        GIT_USER_GID="${GIT_USER_UID}"
-    fi
+	if [ -z "${GIT_USER_GID-}" ]; then
+		GIT_USER_GID="${GIT_USER_UID}"
+	fi
 
-    # Recreate git user
-    deluser "${GIT_USER}"
-    addgroup -g "${GIT_USER_GID}" "${GIT_GROUP}"
-    adduser -h "${GIT_HOME}" -g "git daemon user" -D -s "$(which git-shell)" -G "${GIT_GROUP}" -H -u "${GIT_USER_UID}" "${GIT_USER}"
-    if ! CHPASSWD=$(echo "${GIT_USER}:1q2w3e4r" | chpasswd 2>&1); then
-        echo "$CHPASSWD"
-        exit 1
-    fi
+	# Recreate git user
+	deluser "${GIT_USER}"
+	addgroup -g "${GIT_USER_GID}" "${GIT_GROUP}"
+	adduser -h "${GIT_HOME}" -g "git daemon user" -D -s "$(which git-shell)" -G "${GIT_GROUP}" -H -u "${GIT_USER_UID}" "${GIT_USER}"
+	if ! CHPASSWD=$(echo "${GIT_USER}:1q2w3e4r" | chpasswd 2>&1); then
+		echo "$CHPASSWD"
+		exit 1
+	fi
 fi
 
 if [ -d "${GIT_REPOSITORIES_PATH}" ]; then
-    cd "${GIT_REPOSITORIES_PATH}"/.
+	cd "${GIT_REPOSITORIES_PATH}"/.
 
-    # Fix ownership and permission of repositories
-    chown -R "${GIT_USER}":"${GIT_GROUP}" .
-    find . -type f -exec chmod u=rwX,go=rX '{}' \;
-    find . -type d -exec chmod u=rwx,go=rx '{}' \;
+	# Fix ownership and permission of repositories
+	chown -R "${GIT_USER}":"${GIT_GROUP}" .
+	find . -type f -exec chmod u=rwX,go=rX '{}' \;
+	find . -type d -exec chmod u=rwx,go=rx '{}' \;
 
-    # Symlink repositories to home directory
-    for PATH_ENTRY in ${GIT_REPOSITORIES_PATH}/*; do
-        if [ -d "$PATH_ENTRY" ]; then
-            if su -s /bin/sh - "${GIT_USER}" -c "git -C ${PATH_ENTRY} rev-parse --git-dir >/dev/null"; then
-                ln -sf "${PATH_ENTRY}" "${GIT_HOME}"
-            fi
-        fi
-    done
+	# Symlink repositories to home directory
+	for PATH_ENTRY in ${GIT_REPOSITORIES_PATH}/*; do
+		if [ -d "$PATH_ENTRY" ]; then
+			if su -s /bin/sh - "${GIT_USER}" -c "git -C ${PATH_ENTRY} rev-parse --git-dir >/dev/null"; then
+				ln -sf "${PATH_ENTRY}" "${GIT_HOME}"
+			fi
+		fi
+	done
 else
-    warn "Directory $GIT_REPOSITORIES_PATH not found."
+	warn "Directory $GIT_REPOSITORIES_PATH not found."
 fi
 
 # Replace SSH host keys if path given
 if [ -n "${SSH_HOST_KEYS_PATH-}" ]; then
-    if [ -d "${SSH_HOST_KEYS_PATH}" ]; then
-        rm -rf /etc/ssh/ssh_host_*
-        cp "${SSH_HOST_KEYS_PATH}"/ssh_host_* /etc/ssh/
-    else
-        warn "Directory $SSH_HOST_KEYS_PATH not found."
-        warn "Default SSH host keys will be used instead."
-    fi
+	if [ -d "${SSH_HOST_KEYS_PATH}" ]; then
+		rm -rf /etc/ssh/ssh_host_*
+		cp "${SSH_HOST_KEYS_PATH}"/ssh_host_* /etc/ssh/
+	else
+		warn "Directory $SSH_HOST_KEYS_PATH not found."
+		warn "Default SSH host keys will be used instead."
+	fi
 fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,40 +4,40 @@
 set -eu
 
 info() {
-    echo "$0: $@"
+	echo "$0: $@"
 }
 
 warn() {
-    echo "$0: WARN: $@"
+	echo "$0: WARN: $@"
 }
 
 error() {
-    echo "$0: ERR: $@" 1>&2
+	echo "$0: ERR: $@" 1>&2
 }
 
 ENTRYPOINT_DIR="/entrypoint.d/"
 
 if [ -d "$ENTRYPOINT_DIR" ]; then
-    info "Perform container configuration."
+	info "Perform container configuration."
 
-    find "$ENTRYPOINT_DIR" -follow -type f -print | sort -V | while read -r f; do
-        case "$f" in
-        *.sh)
-            if [ -x "$f" ]; then
-                info "Launch $f."
-                "$f"
-            else
-                warn "Ignored $f, not executable."
-            fi
-            ;;
-        *) warn "Ignored $f." ;;
-        esac
-    done
+	find "$ENTRYPOINT_DIR" -follow -type f -print | sort -V | while read -r f; do
+		case "$f" in
+		*.sh)
+			if [ -x "$f" ]; then
+				info "Launch $f."
+				"$f"
+			else
+				warn "Ignored $f, not executable."
+			fi
+			;;
+		*) warn "Ignored $f." ;;
+		esac
+	done
 
-    info "Container configuration complete."
+	info "Container configuration complete."
 else
-    error "Configuration directory $ENTRYPOINT_DIR not found."
-    exit 1
+	error "Configuration directory $ENTRYPOINT_DIR not found."
+	exit 1
 fi
 
 exec "$@"
